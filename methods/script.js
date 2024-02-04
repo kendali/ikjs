bda8a03ac722159da8e645ca53a1a481205f2c26
kendali <bouchaibkendali@gmail.com>
2024-02-04 17:09:00 +0100

    day2

---
+++ methods/script.js
@@ -0,0 +1,52 @@
+/*
+String Methods , Numbers Methods,Array Methods,
+ + Events
+ String = length , chartAt(number),indexOf(string),
+ toUpperCase() and toLowerCase(), trim(), concat(string1, string2, ...)
+ Numbers = parseInt(string) , parseFloat(string)
+ Array = 
+  push()=> Adds one or more elements to the end of an array
+  pop()=> Removes the last element 
+  unshift()=> Adds one or more elements to the beginning of an array
+  shift()=> Removes the first element from an array and returns it
+  concat(array1, array2, ...)
+*/
+
+
+// const fullName = '   mohamed taha'
+// const mohaName = "kendali"
+// console.log(fullName)
+// console.log(fullName.length)
+// console.log(fullName.charAt(0))
+// console.log(mohaName.charAt(0))
+// console.log(fullName.indexOf('m'))
+// console.log(fullName.toLowerCase())
+// console.log(fullName.toUpperCase())
+// console.log(fullName.trim())
+
+
+// const number = 20.27
+// console.log(number)
+// console.log(parseInt(number))
+// console.log(parseFloat(number))
+
+
+
+// const myArray = ['ahmed','kend','hassan','ilyas']
+const twoArray = ['ahmed',28,36,37,false,'ikram']
+// console.log(myArray)
+// console.log(twoArray)
+// console.log(twoArray.concat(myArray))
+
+// console.log(twoArray.push('8',7,false,'mo','ge'))
+// console.log(twoArray.unshift('ikram','mohamed','ilyas'))
+// twoArray.pop()
+twoArray.shift()
+
+console.log(twoArray)
+
+
+
+
+
+
---
+++ methods/tp.js
@@ -0,0 +1,11 @@
+// 
+
+const para = document.getElementById('para').innerHTML += 'AHMED'.toLowerCase()
+
+const testArr = [34,49,56,32]
+// push + pop + shift + unshift
+
+
+const number = 37.272
+// parseInt + parseFloat
+
